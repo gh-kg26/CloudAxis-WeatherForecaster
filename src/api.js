b8f394995db1830c031c70de257c29554668c51f
Vaishnavi VAishnav <73063827+vaishnavi-vaishnav@users.noreply.github.com>
2023-03-03 12:39:37 +0530

    Adding all local files to github repo

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'af0488f470mshb0df712b58d9d60p17b771jsn2c19963ac3ca',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "35a74f8349994c70af9745bda8c028f8";
+	
+    
